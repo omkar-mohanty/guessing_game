6b18209b317a7341da12fe2a068b0e58242ff0b8
omkar-mohanty <franzohouser@gmail.com>
2022-05-16 11:33:35 +0530

    Rust guessing game

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+fn main() {
+    let secret_number:u32 = rand::thread_rng().gen_range(1..101);
+    println!("Guessing Game");
+   
+    let mut guess = String::new();
+
+    loop{
+
+        println!("Ether a number");
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read input");
+
+
+        let guess:u32 =match guess.trim().parse::<u32>(){
+            Ok(num)=>num,
+            Err(_) => continue,
+        };
+
+        println!("You Guessed {}",guess);
+
+        match guess.cmp(&secret_number){
+            Ordering::Less=>println!("Less THan the number"),
+            Ordering::Greater=>println!("Greater"),
+            Ordering::Equal=>{
+                println!("Correct!!");
+                break;
+            }
+        }
+    }
+}
